0fda912fe25d9f93d39146d9beec3e3f99a0cff8
MonedaCloud IO <mo@monedacloud.io>
2022-08-23 00:32:57 -0400

    Create install-cardano-node.sh

---
+++ install-cardano-node.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+### Cardano Node Build/Install FROM source: 3 OF 3 ###
+### Run first pre-install-os.sh script. ###
+### Run second pre-install-libs.sh script. ###
+
+CNODE_VERSION="1.35.3"
+
+mkdir -p ~/src
+
+cd ~/src
+git clone https://github.com/input-output-hk/cardano-node.git
+cd cardano-node
+git fetch --tags --all
+git checkout tags/$CNODE_VERSION
+echo "with-compiler: ghc-8.10.7" >> cabal.project.local
+cabal build all
+
+mkdir -p ~/.local/bin
+cp -p "$(./scripts/bin-path.sh cardano-node)" ~/.local/bin/
+cp -p "$(./scripts/bin-path.sh cardano-cli)" ~/.local/bin/
+echo 'export PATH="$HOME/.local/bin/:$PATH"' >> ~/.bashrc
+export PATH="$HOME/.local/bin/:$PATH"
+. "${HOME}/.bashrc"
+
+cardano-cli --version
+cardano-node --version
+
+cd /opt/cardano/cnode/scripts
+./deploy-as-systemd.sh
+
+echo 'alias env=/usr/bin/env
+alias cntools=/opt/cardano/cnode/scripts/cntools.sh
+alias gLiveView=/opt/cardano/cnode/scripts/gLiveView.sh
+export CARDANO_NODE_SOCKET_PATH="/opt/cardano/cnode/sockets/node0.socket"
+export CNODE_HOME=/opt/cardano/cnode' >> ~/.bashrc
+. "${HOME}/.bashrc"
+echo 'END'
