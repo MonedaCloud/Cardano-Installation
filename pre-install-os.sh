8ccc7a2250ae11067d2f792358b5ac95928b4261
MonedaCloud IO <mo@monedacloud.io>
2022-08-25 17:53:46 -0400

    Update pre-install-os.sh

--- pre-install-os.sh
+++ pre-install-os.sh
@@ -9,10 +9,10 @@ sudo apt autoremove -y
 
 sudo apt-get install automake build-essential pkg-config libffi-dev libgmp-dev libssl-dev libtinfo-dev libsystemd-dev zlib1g-dev make g++ tmux git jq wget libncursesw5 libtool autoconf liblmdb-dev -y
 
-sudo apt-get install curl libffi7 libgmp10 libncurses-dev libncurses5 libtinfo5 -y
+sudo apt-get install curl libffi7 libgmp10 libncurses-dev libncurses5 libtinfo5 sqlite3 -y
 
 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 
 . "${HOME}/.bashrc"
 
-echo 'Run source ~/.bashrc on the terminal before proceeding to nest step.'
+echo 'Run [ source ~/.bashrc ] on the terminal before proceeding to nest step.'
