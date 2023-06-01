338de956859dfef8e6a9db561bcdfdd6c8ee781c
MonedaCloud IO <mo@monedacloud.io>
2023-05-31 20:38:32 -0400

    Update pre-install-libs.sh

--- pre-install-libs.sh
+++ pre-install-libs.sh
@@ -3,9 +3,9 @@
 ### Cardano Node Libs FROM source: 2 OF 3 ###
 ### Run first pre-install-os.sh script. ###
 
-# Values: RELAY|NODE|UPDATE
+# Values: RELAY|NODE
 MODE='RELAY'
-# Values: mainnet|testnet
+# Values: mainnet|preprod
 NETWORK='mainnet'
 
 
@@ -19,16 +19,6 @@ cabal update
 
 mkdir -p ~/src
 
-cd ~/src
-rm -rf libsodium
-git clone https://github.com/input-output-hk/libsodium libsodium
-cd libsodium
-git checkout dbb48cce5429cb6585c9034f002568964f1ce567
-./autogen.sh
-./configure
-make
-sudo make install
-
 echo " " >> ~/.bashrc
 echo 'export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"' >> ~/.bashrc
 echo 'export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH"' >> ~/.bashrc
@@ -36,8 +26,10 @@ export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
 export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH"
 . "${HOME}/.bashrc"
 
+# Install libsodium-dev native OS libs:
 sudo apt install libsodium-dev
 
+# Install secp256k1 libs:
 cd ~/src
 rm -rf secp256k1
 git clone https://github.com/bitcoin-core/secp256k1 secp256k1
@@ -48,32 +40,47 @@ git checkout ac83be33
 make
 sudo make install
 
-
+# Install Install libsodium fork from IO repositories:
 cd ~/src
-curl -sS -o prereqs.sh https://raw.githubusercontent.com/cardano-community/guild-operators/master/scripts/cnode-helper-scripts/prereqs.sh
-chmod 755 prereqs.sh
+rm -rf libsodium
+git clone https://github.com/input-output-hk/libsodium libsodium
+cd libsodium
+git checkout dbb48cce5429cb6585c9034f002568964f1ce567
+./autogen.sh
+./configure
+make
+sudo make install
+
+# Download and configure Guild deploy script:
+cd ~/src
+curl -sS -o guild-deploy.sh https://raw.githubusercontent.com/cardano-community/guild-operators/master/scripts/cnode-helper-scripts/guild-deploy.sh
+chmod 755 guild-deploy.sh
 
 case "$MODE" in
 'RELAY')
-#RELAY
+# RELAY
 echo "1. Setting up $MODE on $NETWORK"
-./prereqs.sh -n "$NETWORK" -f -s
+./guild-deploy.sh -n "$NETWORK"
 ;;
 'NODE')
-#NODE - INSTALLS CNCLI
+# NODE - INSTALLS CNCLI
 echo "2. Setting up $MODE on $NETWORK"
-./prereqs.sh -n "$NETWORK" -f -s -c
-;;
-'UPDATE')
-#UPGRADE
-echo "3. Setting up $MODE on $NETWORK"
-./prereqs.sh -n "$NETWORK" -s
+# Install CNCLI Libs:
+cd ~/src
+git clone --recurse-submodules https://github.com/cardano-community/cncli
+cd cncli
+git checkout v5.3.2
+cargo install --path . --force
+cncli --version
+# Run Guild deploy script:
+cd ~/src
+./guild-deploy.sh -n "$NETWORK" -s f,c
 ;;
 *)
-echo "MODE: RELAY|NODE|UPDATE \n NETWORK: mainnet|testnet"
+echo "MODE: RELAY|NODE \n NETWORK: mainnet|preprod"
 ;;
 esac
 
 . "${HOME}/.bashrc"
 
-echo 'Run [ source ~/.bashrc ] on the terminal before proceeding to nest step.'
+echo 'Run [ source ~/.bashrc ] on the terminal before proceeding to next step.'
