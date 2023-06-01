f6dfb8ea7af074843033a2e076b36f7241665a69
MonedaCloud IO <mo@monedacloud.io>
2023-05-31 20:36:39 -0400

    Update pre-install-os.sh

--- pre-install-os.sh
+++ pre-install-os.sh
@@ -7,12 +7,26 @@ sudo apt upgrade -y
 sudo apt install vim htop inetutils-ping -y
 sudo apt autoremove -y
 
-sudo apt-get install automake build-essential pkg-config libffi-dev libgmp-dev libssl-dev libtinfo-dev libsystemd-dev zlib1g-dev make g++ tmux git jq wget libncursesw5 libtool autoconf liblmdb-dev -y
+sudo apt-get install automake build-essential pkg-config libffi-dev libgmp-dev libssl-dev libtinfo-dev libsystemd-dev zlib1g-dev make g++ tmux git jq wget libncursesw5 libtool autoconf liblmdb-dev musl-tools -y
 
 sudo apt-get install curl libffi7 libgmp10 libncurses-dev libncurses5 libtinfo5 sqlite3 -y
 
 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 
 . "${HOME}/.bashrc"
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+source $HOME/.cargo/env
+
+rustup install stable
+
+rustup default stable
+
+rustup update
+
+rustup component add clippy rustfmt
+
+rustup target add x86_64-unknown-linux-musl
 
 echo 'Run [ source ~/.bashrc ] on the terminal before proceeding to nest step.'
