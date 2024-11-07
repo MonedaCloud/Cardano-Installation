c3c21ec1080cdbc03a3276755d04e457d4d7071c
Milton Oliva <mo@monedacloud.io>
2024-11-07 15:39:04 -0500

    v10.1.2

--- upgrade-cardano-node-binaries.sh
+++ upgrade-cardano-node-binaries.sh
@@ -5,7 +5,7 @@
 ### Run second upgrade-cardano-node.sh script. ###
 
 # Cardano Node Release:
-CNODE_VERSION="10.1.1"
+CNODE_VERSION="10.1.2"
 
 # Binary source:
 BINARY="https://github.com/IntersectMBO/cardano-node/releases/download/$CNODE_VERSION/cardano-node-$CNODE_VERSION-linux.tar.gz"
--- upgrade-cardano-node.sh
+++ upgrade-cardano-node.sh
@@ -5,7 +5,7 @@
 ### Run second upgrade-cardano-node.sh script. ###
 
 # Cardano Node Release:
-CNODE_VERSION="10.1.1"
+CNODE_VERSION="10.1.2"
 
 ### DO NOT EDIT BELOW THS LINE. ###
 
